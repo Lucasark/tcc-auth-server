7d4d77622e096f205f71e06cdbaf8783c94480cc
lucas.lima <ldslima@rd.com.br>
2023-12-10 16:58:30 -0300

    Form LowerCase

--- src/main/java/tcc/uff/auth/server/model/web/TokenForm.java
+++ src/main/java/tcc/uff/auth/server/model/web/TokenForm.java
@@ -1,6 +1,7 @@
 package tcc.uff.auth.server.model.web;
 
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,8 +13,13 @@ import lombok.Setter;
 @NoArgsConstructor
 public class TokenForm {
 
+    @Getter(AccessLevel.NONE)
     private String email;
 
     @Size(min = 5, message = "Token deve ser no minimo 5")
     private String token;
+
+    public String getEmail() {
+        return email.toLowerCase();
+    }
 }
