d24f29a3ace2b0d8074a5a154ad1ec33c8ab2e42
lucas.lima <ldslima@rd.com.br>
2024-03-02 16:20:56 -0300

    feature/ passoword recover

--- src/main/resources/static/js/document.js
+++ src/main/resources/static/js/document.js
@@ -3,3 +3,11 @@ function submitForm() {
     document.getElementById('myModal').style.display = 'block';
     document.getElementById('loading-container').style.display = 'block';
 }
+
+document.getElementById("submit")
+    .addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.key === 'Enter') {
+            submitForm()
+        }
+    });
